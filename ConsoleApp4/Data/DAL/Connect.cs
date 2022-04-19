bf48cc8e5fafd58ace17fe047357d3fa352df4d0
Shukran Shirinov <tu7nwumeg@aztu.local>
2022-04-19 18:35:18 +0400

    praktikataski

---
+++ ConsoleApp4/Data/DAL/Connect.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp4.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4.DAL
+{
+    class Connect:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=215-16;Database=Blog;Trusted_Connection=TRUE;");
+        }
+       public DbSet<Post> Posts { get; set; }
+
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp4.DAL;
+using ConsoleApp4.Data;
+using System;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Post post = new Post();
+            //post.Title = "salam";
+            //post.Content = "Naber hocam";
+            Connect connect = new Connect();
+            //connect.Posts.Add(post);
+            //connect.SaveChanges();
+            //var dpt = connect.Posts.Find(1);
+            //dpt.Title = "SALAMSALAM1";
+            //connect.SaveChanges();
+            //var find = connect.Posts.FirstOrDefault(x => x.Id == 2);
+            //connect.Posts.Remove(find);
+            //Console.WriteLine($"{ find}Obyekt basari ile silindi");
+            //connect.SaveChanges();
+            var All = connect.Posts.OrderBy(x => x.Title).ToList();
+            foreach (var item in All)
+            {
+                Console.WriteLine(item.Content);
+            }
+            
+        }
+    }
+}
